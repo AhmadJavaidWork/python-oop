92c5329ed5e0729716fbee7c7630f407e71e5c18
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-15 17:37:47 +0500

    rectangle class

---
+++ RectangleClass.py
@@ -0,0 +1,9 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+
+myRectangle = Rectangle(10, 20)
+print('Length of the rectangle is:', myRectangle.length)
+print('Width of the rectangle is:', myRectangle.width)
