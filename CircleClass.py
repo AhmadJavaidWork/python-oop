886449b88aa175bdc4970b84c124b4f79d83c947
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-15 17:41:44 +0500

    circle class

---
+++ CircleClass.py
@@ -0,0 +1,9 @@
+class Circle:
+    def __init__(self, radius, color):
+        self.radius = radius
+        self.color = color
+
+
+myCircle = Circle(10, 'red')
+print('Radius of the circle is:', myCircle.radius)
+print('Color of the circle is:', myCircle.color)
