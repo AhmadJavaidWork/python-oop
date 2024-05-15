5ffc99c2aeaec48ff5e9faca4b5f8bfe4170a9ac
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-15 21:30:40 +0500

    car class non public attribute

---
+++ CarClassNonPublicAttribute.py
@@ -0,0 +1,9 @@
+class Car:
+
+    def __init__(self, brand, model, year):
+        self.brand = brand
+        self.model = model
+        self._year = year
+
+
+my_car = Car("porsche", "o11 Carrera", 2020)
