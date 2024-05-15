3282469f8a37ce0c024b427a019d151ebb6bc132
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-15 17:51:02 +0500

    default parameter

--- CircleClass.py
+++ CircleClass.py
@@ -1,9 +1,9 @@
 class Circle:
-    def __init__(self, radius, color):
+    def __init__(self, radius, color='red'):
         self.radius = radius
         self.color = color
 
 
-my_circle = Circle(10, 'red')
+my_circle = Circle(10)
 print('Radius of the circle is:', my_circle.radius)
 print('Color of the circle is:', my_circle.color)
