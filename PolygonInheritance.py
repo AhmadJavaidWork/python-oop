d1f155c095d1cf8f014a73ce5679fd1582af6d52
AhmadJavaidWork <ahmadjavaidwork@gmail.com>
2024-05-16 19:18:17 +0500

    polygon class inheritance

--- PolygonInheritance.py
+++ PolygonInheritance.py
@@ -1,10 +1,23 @@
 class Polygon:
-    pass
+
+    def __init__(self, num_sides, color):
+        self.num_sides = num_sides
+        self.color = color
 
 
 class Triangle(Polygon):
-    pass
+
+    NUM_SIDES = 3
+
+    def __init__(self, base, height, color):
+        Polygon.__init__(self, Triangle.NUM_SIDES, color)
+        self.base = base
+        self.height = height
 
 
 class Squer(Polygon):
     pass
+
+
+my_traingle = Triangle(5, 4, "Blue")
+print(my_traingle.num_sides)
